456087e02635b54a23453e599b1be1ab56f73714
Talat Mahmud <talatcbf@gmail.com>
2024-03-09 15:31:05 +0600

    img tag changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   basePath: "/octopi",
   output: "export",
   reactStrictMode: true
 };
 
-export default nextConfig;
+module.export = nextConfig;
